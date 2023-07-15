b6d61e384ca9905e9f7128f7da6ae52c9a05e90f
saikumarpb <saipusarla7094@gmail.com>
2023-07-15 14:53:38 +0530

    Refactor course view with loadable recoil state

--- ui/src/Courses/course.tsx
+++ ui/src/Courses/course.tsx
@@ -1,20 +1,30 @@
 import { useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValueLoadable } from 'recoil';
 import { filteredCourse } from '../recoil/courses/atom';
+import Loading from '../components/Loading';
 
 export default function Course() {
   const { courseId } = useParams();
-  const course = useRecoilValue(filteredCourse(courseId!));
-  return (
-    <div className="w-full flex flex-col p-4 items-center gap-4">
-      <div className=" w-full bg-slate-300 flex justify-between rounded">
-        <div className="flex flex-col gap-4 pl-4 justify-center">
-          <div className="text-2xl text-sky-600 font-bold">{course?.title}</div>
-          <div className="text-xl">Rs. {course?.price}</div>
-          <div className="text-xl">{course?.description}</div>
+  const course = useRecoilValueLoadable(filteredCourse(courseId!));
+  switch (course.state) {
+    case 'loading':
+      return <Loading />;
+    case 'hasError':
+      return <div>Something went wrong please try again</div>;
+    case 'hasValue':
+      return (
+        <div className="w-full flex flex-col p-4 items-center">
+          <div className=" w-full bg-slate-300 flex justify-between rounded gap-4">
+            <div className="flex flex-col gap-4 pl-4 justify-center">
+              <div className="text-2xl text-sky-600 font-bold">
+                {course.contents?.title}
+              </div>
+              <div className="text-xl text-green-900">Rs. {course.contents?.price}</div>
+              <div className="text-xl">{course.contents?.description}</div>
+            </div>
+            <img src={course.contents?.imageLink} className="w-1/2 rounded-r" />
+          </div>
         </div>
-        <img src={course?.imageLink} className="w-1/2 rounded-r" />
-      </div>
-    </div>
-  );
+      );
+  }
 }
